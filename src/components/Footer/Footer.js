2fdc420d0b5cd4622ae9d282f682349df08789fc
Joakim Andersson <joakim.aandersson@live.se>
2021-02-03 10:20:45 +0100

    changed few styles and fonts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ function Footer(props) {
     return (
         <div  className={styles.footerContainer}>
             <div className={styles.links}>
-                <a href="https://github.com/JockeHTML"><i className="fab fa-md fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/joakim-andersson-3551851b0/"><i className="fab fa-md fa-linkedin-in"></i></a>
+                <a rel="noreferrer" target="_blank" href="https://github.com/JockeHTML"><i className="fab fa-md fa-github"></i></a>
+                <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/joakim-andersson-3551851b0/"><i className="fab fa-md fa-linkedin-in"></i></a>
             </div>
             <div className={styles.copyright}>
                 <p>JOAKIM ANDERSSON © 2021</p>
