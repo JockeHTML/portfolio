c6104a8c80183a32d12fef78adccdddde8de3fff
Joakim Andersson <joakim.aandersson@live.se>
2021-09-02 18:27:03 +0200

    syntax bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,9 @@ function Header(props) {
     <div className={styles.headerContainer}>
       <div className={styles.headerText}>
         <h1>Hello, i'm Joakim</h1>
-<<<<<<< HEAD
         <h2>Slutliga git test!</h2>
 
         <h3>A self taught web developer</h3>
-=======
-        <h3>A self taught web developer</h3>
-        <h3>Testing GIT on my-new-branch version2!</h3>
->>>>>>> my-test-branch
       </div>
       <div className={styles.imageDiv}>
         <img className={styles.headerImage} src="" alt="" />
