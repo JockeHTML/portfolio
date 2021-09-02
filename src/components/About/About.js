a51c6764dbcd7895aef7962c375c71a15361955f
Joakim Andersson <joakim.aandersson@live.se>
2021-09-02 18:13:29 +0200

    testar git version1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import styles from './About.module.css';
+import React from "react";
+import styles from "./About.module.css";
 
 function About(props) {
-    return (
-        <div className={styles.aboutContainer}>
-            <div className={styles.aboutInfo}>
-                <h1>Who am I?</h1>
-                <p>Im a self taught web developer, focusing fully on front-end. At the start of 2020 i decided to leave my current job and pursue a career in coding. I have always had an interest for technology and computers so it felt natural to try get into the IT business.</p>
-            </div>
-            <div className={styles.aboutImage}>
-                <img alt="about" src='' />            
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.aboutContainer}>
+      <div className={styles.aboutInfo}>
+        <h1>Who am I?</h1>
+        <p>
+          Im a self taught web developer, focusing fully on front-end. At the
+          start of 2020 i decided to leave my current job and pursue a career in
+          coding. I have always had an interest for technology and computers so
+          it felt natural to try get into the IT business.
+        </p>
+      </div>
+      <div className={styles.aboutImage}>
+        <img alt="about" src="" />
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react";
+import styles from "./Header.module.css";
 
 function Header(props) {
-    return (
-        <div className={styles.headerContainer}>
-            <div className={styles.headerText}>
-                <h1>Hello, i'm Joakim</h1>
-                <h3>A self taught web developer</h3>
-            </div> 
-            <div className={styles.imageDiv}>
-                <img className={styles.headerImage} src="" alt=""/>
-            </div>
-            <div className={styles.iconDiv}>
-                <a href="#section-one"><i className="fas fa-lg fa-arrow-circle-down"></i></a>
-            </div> 
-        </div>
-    );
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerText}>
+        <h1>Hello, i'm Joakim</h1>
+        <h2>Testar min nya branch via git version1</h2>
+
+        <h3>A self taught web developer</h3>
+      </div>
+      <div className={styles.imageDiv}>
+        <img className={styles.headerImage} src="" alt="" />
+      </div>
+      <div className={styles.iconDiv}>
+        <a href="#section-one">
+          <i className="fas fa-lg fa-arrow-circle-down"></i>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
